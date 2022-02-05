65a69751359a584c7b9cf497c183c431bb06f148
Oswaldo Escobar <oswaldojeisson.escobarhuisa@everis.nttdata.com>
2022-02-05 08:01:18 -0500

    Initial Commit

---
+++ TestTecnical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTecnical
+//
+//  Created by Oswaldo Jeisson Escobar Huisa on 5/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
